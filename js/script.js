7156ef219afa5520cbcff85e5bad7c63e93eb0af
AngeloDalo <angelo251158@gmail.com>
2021-12-01 15:45:35 +0100

    griglia

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function difficultGame (choise) {
     //console.log (choise)
     if (choise == "Easy") {
         return 49; 
-    } else if (choise = "Medium") {
+    } else if (choise == "Medium") {
         return 81;
-    } else {
+    } else if (choise == "Hard") {
         return 100;
     }
 }
@@ -20,7 +20,15 @@ const level = document.getElementById ("difficoltà");
 
 button.addEventListener('click', function () {
     const element = this;
+    let gridsGame = document.querySelector (".grids-game");
+    gridsGame.innerHTML = "";
     //console.log (level.value);
     let numberSquare = difficultGame (level.value);
     //console.log (numberSquare);
+    for (let i=1; i<=numberSquare; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("square");
+        grid.append (i);
+        gridsGame.append(grid);
+    }
 });
